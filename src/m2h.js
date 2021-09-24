b8aaf71012ef2b0d0489c4ae4080d8c761cf2a3c
ngchikin <ng_chikin@163.com>
2021-09-24 20:24:20 +0800

    add m2h function

--- src/m2h.js
+++ src/m2h.js
@@ -1,7 +1,51 @@
 const chalk = require('chalk');
+const marked = require('marked');
+const path = require('path');
+const fs = require('fs');
+
+const hasSameNameFileInThisPath = (fileName, ext) => {
+  try {
+    fs.accessSync(path.join(__dirname, `${fileName}.${ext}`));
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
+
+const getText = (fileName, ext) => {
+  try {
+    const text = fs.readFileSync(
+      path.join(__dirname, `${fileName}.${ext}`),
+      'utf-8'
+    );
+    return text;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const deleteFileByName = (fileName) => {
+  try {
+    fs.unlinkSync(path.join(__dirname, fileName));
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const m2h = (fileName) => {
-  console.log(chalk.greenBright (fileName));
+  if (!hasSameNameFileInThisPath(fileName, 'md')) {
+    throw new Error(chalk.redBright(`no such ${fileName}.md in src`));
+  }
+
+  if (hasSameNameFileInThisPath(fileName, 'html')) {
+    deleteFileByName(fileName);
+  }
+
+  const htmlText = marked(getText(fileName, 'md'));
+
+  fs.writeFileSync()
+
+  console.log(chalk.greenBright(fileName));
 };
 
 module.exports = m2h;
